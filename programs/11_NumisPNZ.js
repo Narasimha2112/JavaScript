475fc6b8e955ceaedfb5065a8ae00674a75958e7
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2025-11-11 14:08:20 +0530

    Check if a number is positive, negative, or zero.

---
+++ programs/11_NumisPNZ.js
@@ -0,0 +1,11 @@
+// Check if a number is positive, negative, or zero.
+
+let num = parseFloat(prompt("Enter a number: "));
+
+if (num > 0) {
+    document.writeln("The number is positive.");
+} else if (num < 0) {
+    document.writeln("The number is negative.");
+} else {
+    document.writeln("The number is zero.");
+}
