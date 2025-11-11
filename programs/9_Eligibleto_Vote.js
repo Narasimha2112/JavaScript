933732c56872d18337e0387cf8b8224e53691f27
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2025-11-11 14:01:55 +0530

    Check if a person is eligible to vote (age ≥ 18).

---
+++ programs/9_Eligibleto_Vote.js
@@ -0,0 +1,9 @@
+// Check if a person is eligible to vote (age ≥ 18).
+
+let age = prompt("Enter a Your age:");
+
+if (age >= 18) {
+    document.writeln("Your are Eligible to Vote");
+} else {
+    document.writeln("Not Eligible to Vote");
+}
